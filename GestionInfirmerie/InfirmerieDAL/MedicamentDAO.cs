86d3aa9c71dc889c6a75e9ae1adefe5e29b6f35c
DylanNoally <dylan.window@hotmail.fr>
2017-12-17 21:42:08 +0100

    Modification/Suppression Médicament
    Modification/Suppression Médicament

---
+++ GestionInfirmerie/InfirmerieDAL/MedicamentDAO.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InfirmerieBO; // Référence à la couche BO
+using System.Data.SqlClient;
+using System.Data;
+
+namespace InfirmerieDAL
+{
+    public class MedicamentDAO
+    {
+        private static MedicamentDAO unMedicamentDAO;
+
+        // Accesseur en lecture, renvoi une instance
+        public static MedicamentDAO GetUnMedicamentDAO()
+        {
+            if (unMedicamentDAO == null)
+            {
+                unMedicamentDAO = new MedicamentDAO();
+            }
+            return unMedicamentDAO;
+        }
+
+        public static int GetMedicament(Medicament unMedicament)
+        {
+            int resultRequete;
+
+            // Connexion à la BD
+            SqlConnection maConnexion = ConnexionBD.GetConnexionBD().GetSqlConnexion();
+
+            // Création d'une liste vide d'objets Utilisateurs
+            List<Medicament> lesMedicaments = new List<Medicament>();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = maConnexion;
+            cmd.CommandText = "SELECT * FROM MEDICAMENT WHERE Libelle_medicament = @leLibelle";
+
+            cmd.Parameters.Add(new SqlParameter("@leLibelle", SqlDbType.NVarChar, 30));
+
+            cmd.Parameters["@leLibelle"].Value = unMedicament.Libelle;
+
+            SqlDataReader monReader = cmd.ExecuteReader();
+
+            if (monReader.HasRows)
+            {
+                resultRequete = 1;
+            }
+            else
+            {
+                resultRequete = 0;
+            }
+            maConnexion.Close();
+            return resultRequete;
+        }
+
+        public static Medicament GetMedicamentInfos(Medicament unMedicament)
+        {
+            int id = 0;
+            bool archive = false;
+            bool possedeVisite = false;
+
+            // Connexion à la BD
+            SqlConnection maConnexion = ConnexionBD.GetConnexionBD().GetSqlConnexion();
+
+            
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = maConnexion;
+            cmd.CommandText = "SELECT Id_medicament, Archive_medicament, PossedeVisite_medicament FROM MEDICAMENT WHERE Libelle_medicament = @leLibelle";
+
+            cmd.Parameters.Add(new SqlParameter("@leLibelle", SqlDbType.NVarChar, 30));
+
+            cmd.Parameters["@leLibelle"].Value = unMedicament.Libelle;
+
+            SqlDataReader monReader = cmd.ExecuteReader();
+
+            while (monReader.Read())
+            {
+                id = int.Parse(monReader["Id_medicament"].ToString());
+                archive = bool.Parse(monReader["Archive_medicament"].ToString());
+                possedeVisite = bool.Parse(monReader["PossedeVisite_medicament"].ToString());
+                // Création d'un objet Medicament
+                
+            }
+            Medicament leMedicament = new Medicament(id, archive, possedeVisite);
+
+            maConnexion.Close();
+            return leMedicament;
+        }
+
+        public List<string> RecupererMedicaments() // Requête qui permet de récupérer les médicaments de la BDD
+        {
+            // Connexion à la BD
+            SqlConnection maConnexion = ConnexionBD.GetConnexionBD().GetSqlConnexion();
+
+            // Création d'une liste vide d'objets Medicaments
+            List<string> lesMedicaments = new List<string>();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = maConnexion;
+            cmd.CommandText = "SELECT Id_medicament, Libelle_medicament FROM MEDICAMENT ORDER BY Libelle_medicament";
+
+            SqlDataReader monReader = cmd.ExecuteReader();
+
+            while (monReader.Read())
+            {
+                // Ajout de chaque libellé dans la liste Box
+                lesMedicaments.Add(monReader["Libelle_medicament"].ToString());
+            }
+            maConnexion.Close();
+            return lesMedicaments;
+        }
+
+        public static int UpdateMedicament(Medicament unMedicament)
+        {
+            int nbEnr;
+
+            // Connexion à la BD SqlConnection maConnexion = ConnexionBD.GetConnexionBD().GetSqlConnexion(); 
+            SqlConnection maConnexion = ConnexionBD.GetConnexionBD().GetSqlConnexion();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = maConnexion;
+
+            // Requête qui met à jour le médicament avec l'id passé en paramètre
+            cmd.CommandText = "UPDATE MEDICAMENT SET [Libelle_medicament] = @leLibelle, [Archive_medicament] = @archive, [PossedeVisite_medicament] = @possedeVisite WHERE [Id_medicament] = @id";
+
+            // Déclaration des paramètres nécessaire pour protéger la requête des injections SQL
+            cmd.Parameters.Add(new SqlParameter("@id", SqlDbType.Int, 30));
+            cmd.Parameters.Add(new SqlParameter("@leLibelle", SqlDbType.NVarChar, 30));
+            cmd.Parameters.Add(new SqlParameter("@archive", SqlDbType.Bit));
+            cmd.Parameters.Add(new SqlParameter("@possedeVisite", SqlDbType.Bit));
+
+            cmd.Parameters["@id"].Value = unMedicament.Id;
+            cmd.Parameters["@leLibelle"].Value = unMedicament.Libelle;
+            cmd.Parameters["@archive"].Value = unMedicament.Archive;
+            cmd.Parameters["@possedeVisite"].Value = unMedicament.PossedeVisite;
+
+            nbEnr = cmd.ExecuteNonQuery();
+
+            maConnexion.Close(); 
+
+            return nbEnr;
+        }
+
+        public static int SupprimerMedicament(Medicament unMedicament)
+        {
+            int nbEnr;
+
+            // Connexion à la BD
+            SqlConnection maConnexion = ConnexionBD.GetConnexionBD().GetSqlConnexion();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = maConnexion;
+
+            // Requête qui supprime le médicament avec l'id passé en paramètre
+            cmd.CommandText = "DELETE FROM MEDICAMENT WHERE [Id_medicament] = + @id";
+
+            // Déclaration du paramètre @id pour protéger la requête des injections SQL
+            cmd.Parameters.Add(new SqlParameter("@id", SqlDbType.Int, 30));
+            cmd.Parameters["@id"].Value = unMedicament.Id;
+
+            nbEnr = cmd.ExecuteNonQuery();
+
+            maConnexion.Close();
+
+            return nbEnr;
+
+        }
+    }
+}
---
+++ GestionInfirmerie/InfirmerieGUI/FrmModifierMedicament.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using InfirmerieBLL; // Référence à la couche BLL
+using InfirmerieBO;  // Référence à la coucher BO
+namespace InfirmerieGUI
+{
+    public partial class FrmModifierMedicament : Form
+    {
+
+
+        public FrmModifierMedicament(string value)
+        {
+            InitializeComponent();
+
+            // Récupération de la chaîne de connexion à la BD à l'ouverture du formulaire
+            GestionUtilisateurs.SetchaineConnexion(ConfigurationManager.ConnectionStrings["Infirmerie"]);
+
+            // Affichage du libellé sélectionné dans le formulaire précédent
+            txtNomMedicament.Text = value;
+
+            // -------------------------------------------------------------------------------------------
+            // Compare ce qui a été passé à partir de la liste déroulante avec ce qu'il y a dans la BDD
+            // afin d'éviter une quelconque erreur de la réactualisation de la liste déroulante
+            // -------------------------------------------------------------------------------------------
+            int requeteComparaison;
+            requeteComparaison = GestionMedicaments.VerifMedicament(txtNomMedicament.Text);
+
+            if (requeteComparaison != 1)
+            {
+                // ---------------------------------------------------------------------------------------------
+                // Si la requête ne trouve pas le médicament passé par la liste cela affiche un message d'erreur 
+                // et renvoie sur le formulaire précédent afin de resélectionner
+                // ---------------------------------------------------------------------------------------------
+                MessageBox.Show("Une erreur s'est produite, réessayer de modifier un médicament !");
+                FrmGestionMedicaments GestionMedicament = new FrmGestionMedicaments();
+                GestionMedicament.Show();
+                this.Hide();
+            }
+            else
+            {
+                Medicament unMedicament;
+                unMedicament = GestionMedicaments.ExtractInfosMedicament(txtNomMedicament.Text);
+
+                txtIdMedicament.Text = unMedicament.Id.ToString();
+                ckbArchive.Checked = unMedicament.Archive;
+                ckbPossedeVisite.Checked = unMedicament.PossedeVisite;
+            }
+        }
+
+        private void ModifierEleve_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRetour_Click(object sender, EventArgs e)
+        {
+            FrmGestionMedicaments GestionMedicaments = new FrmGestionMedicaments();
+            GestionMedicaments.Show();
+            this.Hide();
+        }
+
+        private void ckbArchive_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnValider_Click(object sender, EventArgs e)
+        {
+            // Variable qui récupère la valeur retourner de la méthode de modification d'un médicament au clic du bouton "Valider"
+            int requeteUpdateReussi = GestionMedicaments.ModifierMedicament(int.Parse(txtIdMedicament.Text), txtNomMedicament.Text, ckbArchive.Checked, ckbPossedeVisite.Checked);
+
+            // Si la variable retourne 0 (false), alors une erreur s'est produite, sinon la modification s'est bien passé
+            if (requeteUpdateReussi != 1)
+            {
+                MessageBox.Show("Une erreur s'est produite, réessayer de modifier un médicament !");
+            }
+            else
+            {
+                MessageBox.Show("La modification du médicament " + txtNomMedicament.Text + "a bien été effectuer !");
+
+                // Redirection vers le formulaire contenant la liste des médicaments
+                FrmGestionMedicaments gestionMedicaments = new FrmGestionMedicaments();
+                gestionMedicaments.Show();
+                this.Hide();
+            }
+        }
+
+        private void txtNomMedicament_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmModifierMedicament_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnSuppression_Click(object sender, EventArgs e)
+        {
+            // Variable qui récupère la valeur retourner de la méthode de suppression d'un médicament au clic du bouton "Supprimer"
+            int requeteDeleteReussi = GestionMedicaments.SuppressionMedicament(int.Parse(txtIdMedicament.Text));
+
+            // Si la variable retourne 0 (false), alors une erreur s'est produite, sinon la suppresion s'est bien passé
+            if (requeteDeleteReussi != 1)
+            {
+                MessageBox.Show("Une erreur s'est produite, réessayer de supprimer un médicament !");
+            }
+            else
+            {
+                MessageBox.Show("Le médicament " + txtNomMedicament.Text + "a bien été effectuer !");
+
+                // Redirection vers le formulaire contenant la liste des médicaments
+                FrmGestionMedicaments gestionMedicaments = new FrmGestionMedicaments();
+                gestionMedicaments.Show();
+                this.Hide();
+            }
+        }
+    }
+}
